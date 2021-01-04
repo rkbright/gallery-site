fce5070b528a44577e702ed1140d9a2ccfa4c814
richard bright <rkbright1@gmail.com>
2021-01-03 18:02:48 -0800

    added a contact page

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslock.com\">support@lenslock.con</a>.")
+	}
 }
 
 func main() {

24aa97e3ec8d3245e7e07fc1e814fa5a83aa7d3e
richard bright <rkbright1@gmail.com>
2021-01-18 14:07:19 -0800

    create table using gorm

--- exp/main.go
+++ exp/main.go
@@ -1,5 +1,69 @@
 package main
 
 import (
-	_ "github.com/lib/pq"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "richard"
+	password = "your_password"
+	dbname   = "lenslocked_dev"
+)
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+	Color string
+}
+
+func main() {
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	if err := db.DB().Ping(); err != nil {
+		panic(err)
+	}
+	db.LogMode(true)
+	db.AutoMigrate(&User{})
+
+	name, email, color := getInfo()
+
+	u := User{
+		Name:  name,
+		Email: email,
+		Color: color,
+	}
+	if err = db.Create(&u).Error; err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", u)
+}
+
+func getInfo() (name, email, color string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What is your name?")
+	name, _ = reader.ReadString('\n')
+	fmt.Println("What is your email address?")
+	email, _ = reader.ReadString('\n')
+	fmt.Println("What is your favorite color?")
+	color, _ = reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	email = strings.TrimSpace(email)
+	color = strings.TrimSpace(color)
+	return name, email, color
+}

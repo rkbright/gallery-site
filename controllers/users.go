d0100b252f55f0fc6dd35d6cbad3f47ce975e45d
richard bright <rkbright1@gmail.com>
2021-01-25 17:45:52 -0800

    added a cookie redirect page

--- controllers/users.go
+++ controllers/users.go
@@ -47,7 +47,8 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintln(w, user)
+	signIn(w, &user)
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
 }
 
 type LoginForm struct {
@@ -72,12 +73,16 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	signIn(w, user)
+	http.Redirect(w, r, "/cookietest", http.StatusFound)
+}
+
+func signIn(w http.ResponseWriter, user *models.User) {
 	cookie := http.Cookie{
 		Name:  "email",
 		Value: user.Email,
 	}
 	http.SetCookie(w, &cookie)
-	fmt.Fprintln(w, user)
 }
 
 func (u *Users) CookieTest(w http.ResponseWriter, r *http.Request) {
